84653cf07f55fbc0c22802c26cf52b9e087169a1
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-08-02 00:11:41 +0500

    hero banner image fixed owl carousel dot nav fix

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
-  $(".carousel-1").owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: false,
-    dots: true,
-    center: true,
-    stagePadding: 0,
-    responsive: {
-      0: {
-        items: 1,
-        stagePadding: 50,
-      },
-      960: {
-        items: 3,
-        stagePadding: 160,
-      },
-    },
-  });
-});
-
 window.onload = function () {
+  $(document).ready(function () {
+    $(".carousel-1").owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: false,
+      dots: true,
+      center: true,
+      stagePadding: 0,
+      responsive: {
+        0: {
+          items: 1,
+          stagePadding: 50,
+        },
+        960: {
+          items: 3,
+          stagePadding: 160,
+        },
+      },
+    });
+  });
+
   //faq's accordion script
   const items = document.querySelectorAll(".accordion button");
   function toggleAccordion() {
